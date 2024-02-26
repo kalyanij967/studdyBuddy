3b53deccb8084448f01301a105b748c91f076712
kalyanij967 <78337074+kalyanij967@users.noreply.github.com>
2024-02-26 21:04:09 +0530

    Add files via upload

---
+++ StuddyBuddy/client/src/components/Nav/Nav.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Nav.css';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Nav = () => {
+    const { user, isAuthenticated, isLoading,loginWithRedirect,logout } = useAuth0();
+  return (
+    <nav>
+        <div className="menuIcons">
+            <br />
+            <ul className="navbar-list">
+                <li>
+                    <NavLink to='/' className="navbar-link">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/about' className="navbar-link">About</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/services' className="navbar-link">Services</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/contact' className="navbar-link">Contact Us</NavLink>
+                </li>
+                <li>
+                    {
+                    isAuthenticated ? (
+                    <li>
+                    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+                    Log Out
+                    </button>
+                    </li>
+                    )
+                    :(
+                    <li>
+                        <button onClick={() => loginWithRedirect()}>Log In</button> 
+                    </li>
+                    )
+                    }
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
---
+++ StuddyBuddy/client/src/components/SubHome/SubHome.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+import './SubHome.css'
+
+const SubHome = () => {
+    
+    return (
+        <>
+        <div className="row container-fluid">
+            <div className="data col-lg-6 d-flex justify-content-center align-items-center 100-w 100-vh">
+              <p>Welcome to Lit LinkUp where studying meets serendipity. Connect with fellow book enthusiasts, find your perfect study partner, and embark on a journey of shared learning. Join us today and elevate your study experience.</p>
+            </div>
+            <div className="image col-lg-6 d-flex justify-content-center ">
+                image
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default SubHome;
