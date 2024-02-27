32a0211382f35c05fe6aa2adc5a35d13b9597d54
unknown <kalyanij967@gmail.com>
2024-02-27 21:11:42 +0530

    modified footer and cards

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,45 +1,47 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
-import logo from '../../images/logo2.png'
+import logof from '../../images/logo.png'
 import { FaInstagram ,FaLinkedin,FaTelegram } from "react-icons/fa6";
+import './Footer.css'
 
 const Footer = () => {
   return (
     <>
         <div className="footer row">
-          <div className="col-2">
-            <NavLink to="/" className='logo'>
-                <img src={logo} alt="logo" className='logo'/>
+          <div className="col-2 align-items-center">
+            <NavLink to="/" className='logof'>
+                <img src={logof} alt="logof" className='logof'/>
             </NavLink>
           </div>
           <div className="col-2 sapience">
-            <h6>Sapience</h6>
+            <h5 className='fw-bold'>Sapience</h5>
             <ul>
-              <li>Home</li>
-              <li>How to use</li>
-              <li>Community</li>
-              <li>Study Together</li>
+              <li><NavLink to='#' className='navlink'>Home</NavLink></li>
+              <li><NavLink to='#' className='navlink'>How to use</NavLink></li>
+              <li><NavLink to='#' className='navlink'>Community</NavLink></li>
+              <li><NavLink to='#' className='navlink'>Study Together</NavLink></li>
             </ul>
           </div>
           <div className="col-2 about">
-            <h6>About</h6>
+            <h5 className='fw-bold'>About</h5>
             <ul>
-              <li>FAQ & Rules</li>
-              <li>About Us</li>
-              <li>Contact Us</li>
+              <li><NavLink to='#' className='navlink'>FAQ & Rules</NavLink></li>
+              <li><NavLink to='#' className='navlink'>About Us</NavLink></li>
+              <li><NavLink to='#' className='navlink'>Contact Us</NavLink></li>
             </ul>
           </div>
           <div className="col-2 legal">
-            <h6>Legal</h6>
+            <h5 className='fw-bold'>Legal</h5>
             <ul>
-              <li>Privacy Policy</li>
-              <li> Terms and Conditions</li>
+              <li><NavLink to='#' className='navlink'>Privacy Policy</NavLink></li>
+              <li><NavLink to='#' className='navlink'>Terms and Conditions</NavLink></li>
             </ul>
             </div>
           <div className="col-3 follow">
-          <FaInstagram />
-          <FaLinkedin />
-          <FaTelegram/>
+            <h5 className='fw-bold'>Follow Us</h5>
+          <NavLink to='#' className='navlink'><FaInstagram className='icon'/></NavLink>
+          <NavLink to='#' className='navlink'><FaLinkedin className='icon'/></NavLink>
+          <NavLink to='#' className='navlink'><FaTelegram className='icon'/></NavLink>
           </div>
         </div>
     </>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink} from 'react-router-dom';
 import Nav from '../Nav/Nav'
 import './Navbar.css'
-import logo from '../../images/logo2.png'
+import logo from '../../images/logo.png'
 
 const Navbar = () => {
   return (
